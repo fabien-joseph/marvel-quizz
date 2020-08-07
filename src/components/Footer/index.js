6574a195e12a014733dc2d112937e2232dd6d42d
Fabien Joseph <styleure@ascentia.fr>
2020-08-07 19:28:48 +0200

    Creation of the main components

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className={"footer-container"}>
+                <p>Projet réalisé par Fabien en suivant le cours de Donkey Geek</p>
+                <p>Les icônes Wolverine, Iron Man, SpiderMan et BatMan sont prises sur iconfinder.com</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
