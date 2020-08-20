3ac386aaf598b418b9234c989ec9f2ea2be8d1d9
Fabien Joseph <styleure@ascentia.fr>
2020-08-20 18:03:04 +0200

    Correct a problem with names of class during reflection

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -13,12 +13,31 @@ const Landing = () => {
         }, 3000);
     }, [])
 
+    const setLeftImg = () => {
+        refWolverine.current.classList.add("leftImg");
+
+    }
+
+
+    const setRightImg = () => {
+        refWolverine.current.classList.add("rightImg");
+    }
+
+
+    const clearImg = () => {
+        if (refWolverine.current.classList.contains("rightImg"))
+            refWolverine.current.classList.remove("rightImg");
+        else if (refWolverine.current.classList.contains("leftImg"))
+            refWolverine.current.classList.remove("leftImg");
+
+    }
+
     const displayBtn = btn && (
         <Fragment>
-            <div className={"leftBox"}>
+            <div onMouseOut={clearImg} onMouseOver={setLeftImg} className={"leftBox"}>
                 <button className={"btn-welcome"}>Inscription</button>
             </div>
-            <div className={"rightBox"}>
+            <div onMouseOut={clearImg} onMouseOver={setRightImg} className={"rightBox"}>
                 <button className={"btn-welcome"}>Connexion</button>
             </div>
         </Fragment>
